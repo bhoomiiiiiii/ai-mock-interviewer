9cdd9905c1b2f1706470e9ef94a9ae6d8ffd6d3d
bhoomi arora <bhoomiarora864@gmail.com>
2026-06-06 13:07:30 +0530

    update api url

--- ai-mock-interviewer/client/vite.config.js
+++ ai-mock-interviewer/client/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": "http://localhost:5000",
-    },
+  proxy: {
+    "/api": "https://ai-mock-interviewer-sbk7.onrender.com",
   },
+},
 });
